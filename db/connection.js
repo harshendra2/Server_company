f9cb0e942ad5916e2050d68afcb720d0327bb79a
harshendra raj <harsendraraj20@gmail.com>
2023-12-13 20:17:36 +0530

    server not connection

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
-const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE;
 
 mongoose
-  .connect("mongodb+srv://harsendraraj20:3PhNDGDHb3fxlLEd@cluster0.idcuttr.mongodb.net/?retryWrites=true&w=majority", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
+  .connect("mongodb+srv://harsendraraj20:3PhNDGDHb3fxlLEd@cluster0.idcuttr.mongodb.net/?retryWrites=true&w=majority")
   .then(() => console.log("Database connected"))
   .catch((error) => {
     console.log("Error:", error);
